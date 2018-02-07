bab8acf7ef93e7bfb5dda8b5bc897a966811e417
Jason Lee <jason.khn.lee@gmail.com>
2018-02-07 16:45:33 -0500

    Registration screen

--- dream-frontend/src/Register.js
+++ dream-frontend/src/Register.js
@@ -4,7 +4,13 @@ export class Register extends React.Component {
     render() {
         return (
             <div className="login-clean">
-                <h1>Welcome to the registration page!</h1>
+                <form method="post">
+                    <h2 className="sr-only">Registration Form</h2>
+                    <div className="illustration"><img className="img-responsive" src="https://static.wixstatic.com/media/ca0178_35f7a49f9b32404b953369516a9d55f0.png/v1/fill/w_800,h_539,al_c/ca0178_35f7a49f9b32404b953369516a9d55f0.png" alt="Dream It Forward Logo"/></div>
+                    <div className="form-group"><input className="form-control" type="email" name="email" placeholder="Email" /></div>
+                    <div className="form-group"><input className="form-control" type="password" name="password" placeholder="Password" /></div>
+                    <div className="form-group"><button className="btn btn-primary btn-block" type="submit" style={{ backgroundColor: "rgb(34,167,95)" }}>Register</button></div>
+                </form>
             </div>
         );
     }
