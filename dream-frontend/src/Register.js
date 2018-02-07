3fa53d8ebeb74b6eabe200dae35c1b0215ca6d3c
Jason Lee <jason.khn.lee@gmail.com>
2018-02-07 14:11:40 -0500

    Added react router

---
+++ dream-frontend/src/Register.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export class Register extends React.Component {
+    render() {
+        return (
+            <div className="login-clean">
+                <h1>Welcome to the registration page!</h1>
+            </div>
+        );
+    }
+}
+
