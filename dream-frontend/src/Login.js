7a7ac79a8e8b190a384a1df172ce6fcbdfb3a4f3
Jason Lee <jason.khn.lee@gmail.com>
2018-02-13 19:26:01 -0500

    Modernization

--- dream-frontend/src/Login.js
+++ dream-frontend/src/Login.js
@@ -4,7 +4,7 @@ export class Login extends React.Component {
     render() {
         return (
             <div className="login-clean">
-                <form action="/api/login" method="post">
+                <form>
                     <h2 className="sr-only">Login Form</h2>
                     <div className="illustration"><img className="img-responsive" src="https://static.wixstatic.com/media/ca0178_35f7a49f9b32404b953369516a9d55f0.png/v1/fill/w_800,h_539,al_c/ca0178_35f7a49f9b32404b953369516a9d55f0.png" alt="Dream It Forward Logo"/></div>
                     <div className="form-group"><input className="form-control" type="email" name="email" placeholder="Email" /></div>
