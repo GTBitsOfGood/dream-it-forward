5ea8fef2755101b6d9194c1ca5d7d050436522d9
liebasam <l.samuel.cook@gmail.com>
2018-02-13 19:44:26 -0500

    added userdata query

--- dream-backend/routes/userdata.js
+++ dream-backend/routes/userdata.js
@@ -1,18 +1,24 @@
 ﻿var express = require('express');
 var router = express.Router();
+const User = require('../models/user.js');
 
-/* GET users listing. */
+/* GET userdata */
 router.get('/userdata', function (req, res, next) {
     if (!req.body || !req.body.username) {
         return res.status(400).json({ status: 'Unable to find parameters' });
     }
-    // Check if the username exists
-    userExists = false;
-    if (!userExists) {
-        return res.status(200).json({ status: 'username invalid' });
-    }
-    // Return the userdata
-    return res.status(200).json({ status: 'success' });
+    User.findOne(
+        { username: req.body.username },                // What to search for
+        { username: true, isMentor: true, meta: true }, // What to return
+        (err, user) => {
+        if (err) {                                      // Database error
+            return res.status(500).send(err);
+        }
+        if (!user) {                                    // User not found
+            return res.status(200).json({ status: 'username not found' });
+        }
+        return res.status(200).send(user);              // Return userdata
+    });
 });
 
 module.exports = router;
