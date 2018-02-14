f32aef1290a8889a4fbe91581dc2a4b3e6cf7202
liebasam <l.samuel.cook@gmail.com>
2018-02-13 19:26:37 -0500

    Barebones userdata route

---
+++ dream-backend/routes/userdata.js
@@ -0,0 +1,18 @@
+﻿var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/userdata', function (req, res, next) {
+    if (!req.body || !req.body.username) {
+        return res.status(400).json({ status: 'Unable to find parameters' });
+    }
+    // Check if the username exists
+    userExists = false;
+    if (!userExists) {
+        return res.status(200).json({ status: 'username invalid' });
+    }
+    // Return the userdata
+    return res.status(200).json({ status: 'success' });
+});
+
+module.exports = router;
