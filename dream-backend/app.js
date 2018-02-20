28b20f53d0105db87df4706b5bdc1ec798a26ca6
kevintian <kevintian@gatech.edu>
2018-02-20 13:54:56 -0500

    Added mongodb connection

--- dream-backend/app.js
+++ dream-backend/app.js
@@ -2,13 +2,27 @@ var express = require('express');
 var bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
+//Local Imports
 var userdata = require('./routes/userdata');
 var api = require('./routes/api');
+require('dotenv').config(); // load env vars
 
 var app = express();
+
+
+//Connect to MongoDB
 // mongoose.connect('');
 // var db = mongoose.connection;
 // db.on('error', console.error.bind(console, 'connection error:'));
+mongoose.connect(process.env.MONGODB_URI);
+mongoose.connection.on('connected', () => {
+  console.log("Connection open");
+});
+
+mongoose.connection.on('error', (err) => {
+  console.log("Mongoose connection error");
+});
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
