42c16080a4787ef7bc3eb5b3cbb0c5d73df708f7
Jason Lee <jason.khn.lee@gmail.com>
2018-02-27 18:28:40 -0500

    Login page image

--- dream-frontend/src/Login/Login.js
+++ dream-frontend/src/Login/Login.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Login.css';
-import { observer } from 'mobx-react'
+import { observer } from 'mobx-react';
+const dreamLogo = require('./dream-logo.png');
 
 @observer
 export class Login extends React.Component {
@@ -31,7 +32,7 @@ export class Login extends React.Component {
             <div className="login-clean">
                 <form>
                     <h2 className="sr-only">Login Form</h2>
-                    <div className="illustration"><img className="img-responsive" src="https://static.wixstatic.com/media/ca0178_35f7a49f9b32404b953369516a9d55f0.png/v1/fill/w_800,h_539,al_c/ca0178_35f7a49f9b32404b953369516a9d55f0.png" alt="Dream It Forward Logo" /></div>
+                    <div className="illustration"><img className="img-responsive" src={dreamLogo} alt="Dream It Forward Logo" /></div>
                     <div className="form-group"><input value={store.email} onChange={this.onChange} className="form-control" type="email" name="email" placeholder="Email" /></div>
                     <div className="form-group"><input value={store.password} onChange={this.onChange} className="form-control" type="password" name="password" placeholder="Password" /></div>
                     <div className="form-group"><button className="btn btn-primary btn-block" onClick={(e) => this.onSubmit(e)} style={{ backgroundColor: "rgb(34,167,95)" }}>Login</button></div>
