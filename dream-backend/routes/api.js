8579ec62a5bd57288b9dce41244da6a1b9088a62
liebasam <l.samuel.cook@gmail.com>
2018-03-05 17:37:06 -0500

    GET and PUT for /users route

--- dream-backend/routes/api.js
+++ dream-backend/routes/api.js
@@ -4,9 +4,11 @@ var router = express.Router();
 const register = require('../controllers/register');
 const login = require('../controllers/login');
 const auth = require('../controllers/auth');
+const users = require('../controllers/users');
 
 router.use('/login', login);
 router.use('/register', register);
 router.use('/token', auth);
+router.use('/users', users);
 
 module.exports = router;
