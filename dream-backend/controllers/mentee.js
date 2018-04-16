c4b0392f1e8bb8d322b60cf6b44c2d67fe7bb6f0
brow <anishvisaria98@gmail.com>
2018-04-16 02:28:42 -0400

    mentee app done

--- dream-backend/controllers/mentee.js
+++ dream-backend/controllers/mentee.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const jwt = require('jsonwebtoken')
 require('dotenv').config();
 
 const User = require('../models/user.js');
--- dream-backend/controllers/user.js
+++ dream-backend/controllers/user.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 require('dotenv').config();
+const jwt = require('jsonwebtoken')
 
 const User = require('../models/user.js');
 
