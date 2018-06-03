1702098c7a97ba38ced61e75b2ced136c4735bb3
brow <anishvisaria98@gmail.com>
2018-06-02 20:46:09 -0400

    disable dev

--- dream-frontend/webpack.config.js
+++ dream-frontend/webpack.config.js
@@ -12,7 +12,11 @@ module.exports = {
     publicPath: '/'
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin()
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    }),
+    new webpack.optimize.UglifyJsPlugin()
   ],
   resolve: {
     extensions: ['.js', '.jsx']
